3d6ab1d48760721c7a3b860817694377490133a0
mry <humi19793@gmail.com>
2026-06-24 11:13:29 +0900

    Registryのテストコードを作成

--- src/test/java/com/github/mry114/skyMMOCore/test/RegistryTest.java
+++ src/test/java/com/github/mry114/skyMMOCore/test/RegistryTest.java
@@ -1,4 +1,23 @@
 package com.github.mry114.skyMMOCore.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.registry.Registry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RegistryTest {
+    private static final Status MAX_HEALTH = new MaxHealth();
+
+    @Test
+    public void registeredObjectCanBeRetrieved() {
+        Registry<Status> STATUS = new Registry<Status>(MAX_HEALTH);
+
+        STATUS.register(MAX_HEALTH);
+
+        Status result = STATUS.get(MAX_HEALTH);
+
+        assertEquals(MAX_HEALTH, result);
+    }
 }
